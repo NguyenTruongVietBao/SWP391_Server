0da59c2102ea7dec3e1c610b60a490d2d1e07ad3
Nguyễn Trương Viết Bảo <nguyentvbao2403@gmail.com>
2024-06-01 16:14:01 +0700

    add security, prevent it

--- src/main/java/com/math/primarySchoolMath/PrimarySchoolMathApplication.java
+++ src/main/java/com/math/primarySchoolMath/PrimarySchoolMathApplication.java
@@ -2,8 +2,9 @@ package com.math.primarySchoolMath;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class PrimarySchoolMathApplication {
 
 	public static void main(String[] args) {
