e3e213ce4f780cc63228a2c445a025adec9274b2
duypon2601 <154984098+duypon2601@users.noreply.github.com>
2024-06-15 14:42:32 +0700

    add question"

---
+++ src/main/java/com/math/mathcha/dto/request/QuestionDTO.java
@@ -0,0 +1,27 @@
+package com.math.mathcha.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class QuestionDTO {
+
+    private long question_id;
+
+    private String content;
+
+    private String title;
+
+    private List<String> option;
+
+    private String correctAnswer;
+
+
+}
---
+++ src/main/java/com/math/mathcha/entity/Question/Question.java
@@ -0,0 +1,39 @@
+package com.math.mathcha.entity.Question;
+
+import com.math.mathcha.entity.Topic;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "question")
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "question_id")
+    private Long question_id; // sửa lại từ content sang id
+
+    @Column(name = "content")
+    private String content; // thêm trường content
+
+    @Column(name = "title")
+    private String title; // thêm trường title
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<QuestionOption> options;
+
+    @Column(name = "correct_answer")
+    private String correctAnswer;
+
+    @ManyToOne
+    @JoinColumn(name = "topic_id", nullable = false)
+    private Topic topic; // sửa lại từ question sang Lesson
+}
