2a968886365d89124e5ba3cde90725a31b84a49f
duypon2601 <154984098+duypon2601@users.noreply.github.com>
2024-06-26 20:45:32 +0700

    update quiz evaluate

--- src/main/java/com/math/mathcha/controller/QuizController/QuizController.java
+++ src/main/java/com/math/mathcha/controller/QuizController/QuizController.java
@@ -20,30 +20,6 @@ public class QuizController {
 
     private QuizService quizService;
 
-//    @GetMapping("/question")
-//    public ResponseEntity<List<QuestionDTO>> getQuizQuestions() {
-//        List<QuestionDTO> questions = quizService.getQuizQuestions();
-//        return ResponseEntity.ok(questions);
-//    }
-//
-//    @GetMapping("/chapter/{chapterId}")
-//    public ResponseEntity<List<QuestionDTO>> getQuizForChapter(@PathVariable int chapterId, @RequestParam int questionsPerTopic) {
-//        List<QuestionDTO> quiz = quizService.generateQuizForChapter(chapterId, questionsPerTopic);
-//        return ResponseEntity.ok(quiz);
-//    }
-//
-//    @PostMapping("/{quizId}/evaluate")
-//    public ResponseEntity<QuizResultDTO> evaluateQuiz(@PathVariable Long quizId, @RequestBody EvaluateQuizRequest request) {
-//        QuizResultDTO result = quizService.evaluateQuiz(quizId, request.getUserId(), request.getQuizDTO());
-//        return ResponseEntity.ok(result);
-//    }
-//    @Getter
-//    @Setter
-//    public static class EvaluateQuizRequest {
-//        private Long userId;
-//        private QuizDTO quizDTO;
-//    }
-
     @GetMapping("/questions")
     public List<QuestionDTO> getQuizQuestions() {
         return quizService.getQuizQuestions();
