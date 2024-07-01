28b9c9b871a770f47b1dafa35befe41d2d1870ce
Nguyễn Đình Minh Đăng <minhdang22082003@gmail.com>
2024-07-01 22:13:50 +0700

    increase performance

--- src/main/java/com/math/mathcha/repository/TopicRepository/TopicRepository.java
+++ src/main/java/com/math/mathcha/repository/TopicRepository/TopicRepository.java
@@ -11,4 +11,7 @@ import java.util.List;
 public interface TopicRepository extends JpaRepository<Topic,Integer> {
     @Query("SELECT t FROM Topic t WHERE t.chapter.chapter_id = :chapter_id")
     List<Topic> findTopicsByChapterId(@Param(value = "chapter_id") int chapter_id);
+
+    @Query("SELECT l.chapter FROM Topic l WHERE l.topic_id = :topic_id")
+    Chapter findChapterIdByTopicId(@Param("topic_id") int topic_id);
 }
